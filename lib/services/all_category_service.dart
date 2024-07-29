9248ca1e82283e424c21966cde62fca22abb4366
ibrahimagdy <iibbrraahhimm00@gmail.com>
2024-07-29 23:31:21 +0300

    service for all category

---
+++ lib/services/all_category_service.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class AllCategoryService{
+
+  Future<List<dynamic>> getAllCategory() async{
+    http.Response response = await http.get(Uri.parse('https://fakestoreapi.com/products/categories'));
+    List<dynamic> data =  jsonDecode(response.body);
+    return data;
+  }
+}
